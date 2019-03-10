3c943026a98418abb0c7856f5266864e537be033
lishuangg <1241982864@qq.com>
2019-03-10 21:59:51 +0800

    更新练习1234

--- 课程练习/LS26/NodePackDemo/test02.js
+++
@@ -1,8 +0,0 @@
-// var test01 = require("./test01");
-// console.log(test01.a);
-// console.log(test01.b);
-// console.log(test01.c());
-
-var Person = require("./Person");
-var p = new Person("jack",23);
-p.show();
